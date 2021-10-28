83d6d56b8f2ebd1c867d59223ffec6d86f7e3964
Jaume Cadafalch <84527588+jcadafalch@users.noreply.github.com>
2021-10-28 13:43:25 +0200

    binding

--- app/src/main/java/cat/copernic/jcadafalch_aramirez/lacantinadeprodis/MainActivity.kt
+++ app/src/main/java/cat/copernic/jcadafalch_aramirez/lacantinadeprodis/MainActivity.kt
@@ -2,10 +2,15 @@ package cat.copernic.jcadafalch_aramirez.lacantinadeprodis
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import cat.copernic.jcadafalch_aramirez.lacantinadeprodis.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
 }
