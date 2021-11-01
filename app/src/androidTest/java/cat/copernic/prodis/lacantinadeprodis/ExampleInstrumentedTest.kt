714b4bbecc603844ac3eaffeae28a38a6363a35f
Jaume Cadafalch <84527588+jcadafalch@users.noreply.github.com>
2021-11-01 12:07:31 +0100

    Pantalla principal + navegació de les altres

--- app/src/androidTest/java/cat/copernic/prodis/lacantinadeprodis/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/cat/copernic/prodis/lacantinadeprodis/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package cat.copernic.jcadafalch_aramirez.lacantinadeprodis
+package cat.copernic.prodis.lacantinadeprodis
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
