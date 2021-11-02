f2eb7ed4f62b5472b5a64aa47d815b4e68612873
Jaume Cadafalch <84527588+jcadafalch@users.noreply.github.com>
2021-11-02 13:04:19 +0100

    fragments

---
+++ app/src/main/java/cat/copernic/prodis/lacantinadeprodis/PantallaPrincipal.kt
@@ -0,0 +1,17 @@
+package cat.copernic.prodis.lacantinadeprodis
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+
+class PantallaPrincipal : Fragment() {
+    /*override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        val binding = DataBindingUtil.inflate<>(inflater,
+            R.layout.fragment_pantalla_principal,container,false)
+        return binding.root
+    }*/
+}
---
+++ app/src/main/java/cat/copernic/prodis/lacantinadeprodis/PantallaRecuperarContrasenya2.kt
@@ -0,0 +1,19 @@
+package cat.copernic.prodis.lacantinadeprodis
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class PantallaRecuperarContrasenya2 : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_pantalla_recuperar_contrasenya2, container, false)
+    }
+
+}
