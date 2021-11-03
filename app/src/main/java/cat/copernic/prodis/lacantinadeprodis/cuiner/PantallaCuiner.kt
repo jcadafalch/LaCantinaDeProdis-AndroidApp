065893078db4339fac6ed37bfbbba5ee2ba5f545
Jaume Cadafalch <84527588+jcadafalch@users.noreply.github.com>
2021-11-03 12:16:33 +0100

    rename pantallaComandes -> pantallaCuiner

--- app/src/main/java/cat/copernic/prodis/lacantinadeprodis/cuiner/PantallaCuiner.kt
+++ app/src/main/java/cat/copernic/prodis/lacantinadeprodis/cuiner/PantallaCuiner.kt
@@ -1,4 +1,4 @@
-package cat.copernic.prodis.lacantinadeprodis.comandes
+package cat.copernic.prodis.lacantinadeprodis.cuiner
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -7,13 +7,13 @@ import android.view.View
 import android.view.ViewGroup
 import cat.copernic.prodis.lacantinadeprodis.R
 
-class PantallaComandes : Fragment() {
+class PantallaCuiner : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_pantalla_comandes, container, false)
+        return inflater.inflate(R.layout.fragment_pantalla_cuiner, container, false)
     }
 }
