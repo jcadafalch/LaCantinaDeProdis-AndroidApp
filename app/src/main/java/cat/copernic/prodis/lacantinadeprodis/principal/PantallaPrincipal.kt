c320a031c80ef7c38629793545ea83cdf437b753
Jaume Cadafalch <84527588+jcadafalch@users.noreply.github.com>
2021-11-03 13:27:57 +0100

    PantallaPrincipal.kt + MainActivity.kt descommentat

--- app/src/main/java/cat/copernic/prodis/lacantinadeprodis/principal/PantallaPrincipal.kt
+++ app/src/main/java/cat/copernic/prodis/lacantinadeprodis/principal/PantallaPrincipal.kt
@@ -1,12 +1,19 @@
 package cat.copernic.prodis.lacantinadeprodis.principal
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import cat.copernic.prodis.lacantinadeprodis.R
+import cat.copernic.prodis.lacantinadeprodis.databinding.FragmentPantallaPrincipalBinding
 
 class PantallaPrincipal : Fragment() {
-    /*override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        val binding = DataBindingUtil.inflate<>(inflater,
+        val binding = DataBindingUtil.inflate<FragmentPantallaPrincipalBinding>(inflater,
             R.layout.fragment_pantalla_principal,container,false)
         return binding.root
-    }*/
+    }
 }
