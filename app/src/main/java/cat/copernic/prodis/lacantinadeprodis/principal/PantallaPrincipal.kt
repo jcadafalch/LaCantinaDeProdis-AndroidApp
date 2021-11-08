1fb0a0161e3dc2c121249bde023d7459b8305661
Jaume Cadafalch <84527588+jcadafalch@users.noreply.github.com>
2021-11-08 12:46:43 +0100

    fragment_pantalla_administrador_principal.xml fragment_pantalla_inici_sessio_caixer_cuiner_cambrer.xml fragment_pantalla_inici_sessio_client_admin.xml
    inici fragment_pantalla_registre.xml

--- app/src/main/java/cat/copernic/prodis/lacantinadeprodis/principal/PantallaPrincipal.kt
+++ app/src/main/java/cat/copernic/prodis/lacantinadeprodis/principal/PantallaPrincipal.kt
@@ -1,19 +1,47 @@
 package cat.copernic.prodis.lacantinadeprodis.principal
 
 import android.os.Bundle
+import android.renderscript.ScriptGroup
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import cat.copernic.prodis.lacantinadeprodis.R
+import cat.copernic.prodis.lacantinadeprodis.databinding.ActivityMainBinding
 import cat.copernic.prodis.lacantinadeprodis.databinding.FragmentPantallaPrincipalBinding
+
+private lateinit var binding: FragmentPantallaPrincipalBinding
 
 class PantallaPrincipal : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         val binding = DataBindingUtil.inflate<FragmentPantallaPrincipalBinding>(inflater,
             R.layout.fragment_pantalla_principal,container,false)
+
+        binding.btnPprincipalClient.setOnClickListener{
+            //view: View -> view.findNavController().navigate((R.id.action_pantalla_principal_to_pantalla_inici_sessio_client_admin))
+        }
+
+        binding.btnPprincipalCambrer.setOnClickListener {
+
+        }
+
+        binding.btnPprincipalCaixer.setOnClickListener {
+
+        }
+
+        binding.btnPprincipalCuiner.setOnClickListener {
+
+        }
+
+        binding.btnPprincipalAdministrador.setOnClickListener {
+
+        }
         return binding.root
     }
+
+
+
 }
