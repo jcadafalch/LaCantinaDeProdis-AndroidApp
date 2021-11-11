968af5568492dff5871be30e89163f0898226fcf
ramirez.masagu.adrian <ramirez.masagu.adrian@alumnat.copernic.cat>
2021-11-11 11:43:00 +0100

    Primera versió de navegació.

--- app/src/main/java/cat/copernic/prodis/lacantinadeprodis/MainActivity.kt
+++ app/src/main/java/cat/copernic/prodis/lacantinadeprodis/MainActivity.kt
@@ -1,16 +1,29 @@
 package cat.copernic.prodis.lacantinadeprodis
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import cat.copernic.prodis.lacantinadeprodis.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
 
-    /*override fun onCreate(savedInstanceState: Bundle?) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-    }*/
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this,R.layout.activity_main)
+
+        //val navController = this.findNavController(R.id.myNavHostFragment)
+
+        //Fletxa enderrere i faltaria metode onSupportNavigateUp
+        //NavigationUI.setupActionBarWithNavController(this, navController)
+
+    }
 }
