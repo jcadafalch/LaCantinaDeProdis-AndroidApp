b8d64c3185fa711576a293bf41cfbab2724e58bf
Jaume Cadafalch <84527588+jcadafalch@users.noreply.github.com>
2021-11-17 19:39:28 +0100

    correcció format del codi Pantalla inici sessió

--- app/src/main/java/cat/copernic/prodis/lacantinadeprodis/principal/PantallaIniciSessioClientAdmin.kt
+++ app/src/main/java/cat/copernic/prodis/lacantinadeprodis/principal/PantallaIniciSessioClientAdmin.kt
@@ -3,8 +3,6 @@ package cat.copernic.prodis.lacantinadeprodis.principal
 import android.app.AlertDialog
 import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
-import android.provider.ContactsContract
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -15,7 +13,6 @@ import androidx.navigation.findNavController
 import cat.copernic.prodis.lacantinadeprodis.R
 import cat.copernic.prodis.lacantinadeprodis.activities.AdministradorActivity
 import cat.copernic.prodis.lacantinadeprodis.activities.CambrerActivity
-import cat.copernic.prodis.lacantinadeprodis.administrador.PantallaAdministradorPrincipal
 import cat.copernic.prodis.lacantinadeprodis.databinding.FragmentPantallaIniciSessioClientAdminBinding
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.ktx.firestore
@@ -39,75 +36,25 @@ class PantallaIniciSessioClientAdmin : Fragment() {
         val args = PantallaIniciSessioClientAdminArgs.fromBundle(requireArguments())
         val usertype = args.usertype
 
-        /*binding.btnPiniciarSessioClient.setOnClickListener {
-            println("set on clic listener")
-            if (binding.dtTxtPIniciarSessioClientDni.text.isNotEmpty() &&
-                binding.dtTxtPIniciarSessioClientPassword.text.isNotEmpty()){
-                println("not empty")
-                getEmail(binding.dtTxtPIniciarSessioClientDni.text.toString())
-                //val email = Email
-
-                val password = binding.dtTxtPIniciarSessioClientPassword.text.toString() + "prodis"
-                println("PASWORD GETT $password")
-
-                val email = Email
-                println("EMAIL GETT $email")
-
-                if (email != "null"){
-                    println("Email not NULL $email")
-                    //if (checkUserType(binding.dtTxtPIniciarSessioClientDni.text.toString(), usertype)){
-                        FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password).addOnCompleteListener {
-                            if (it.isSuccessful){
-                                println("IS SUCCESSFUL")
-                                Toast.makeText(context, "Has iniciat sessió", Toast.LENGTH_SHORT).show()
-                                when (usertype){
-                                    "client" -> showCambrerClient(usertype)
-                                    "cambrer" -> showCambrerClient(usertype)
-                                    "caixer" -> showCaixer()
-                                    "cuiner" -> showCuiner()
-                                    "admin" -> showAdmin()
-                                    else -> {
-                                        Toast.makeText(context, "Error en l'inici de sessió", Toast.LENGTH_SHORT).show()
-                                    }
-                                }
-                            }else{
-                                showAlert("Inici de sessió incorrecte")
-                            }
-
-
-                        }
-                    /*}else{
-                        showAlert("Aquest usuari no és $usertype")
-                    }*/
-                }
-            }else{
-                // dt DNI i/o paswd son empty
-
-                showAlert("DNI i/o contrasenya són buits")
-            }
-        }*/
-
         binding.btnPiniciarSessioClient.setOnClickListener {
             val dni = binding.dtTxtPIniciarSessioClientDni.text.toString().uppercase()
             var passwd = binding.dtTxtPIniciarSessioClientPassword.text.toString()
             if (binding.dtTxtPIniciarSessioClientDni.text.isNotEmpty() &&
                 binding.dtTxtPIniciarSessioClientPassword.text.isNotEmpty()
             ) {
-                //TODO afegir metodes de comprovació de dni valid
-
                 passwd += "prodis"
                 db.collection("users").get().addOnSuccessListener { result ->
-                    // Recorrer t0do el documento buscando si coincide con algún DNI
                     for (document in result) {
-                        if(document.id == dni){
+                        if (document.id == dni) {
                             auth.signInWithEmailAndPassword(
                                 document.get("email").toString(),
-                                passwd,).addOnCompleteListener {
-                                    if (it.isSuccessful){
-                                        startActivity(usertype)
-                                    }else{
-                                        showAlert("Error en inici de sessió")
-                                    }
+                                passwd,
+                            ).addOnCompleteListener {
+                                if (it.isSuccessful) {
+                                    startActivity(usertype)
+                                } else {
+                                    showAlert("Error en inici de sessió")
+                                }
                             }
                         }
                     }
@@ -117,7 +64,6 @@ class PantallaIniciSessioClientAdmin : Fragment() {
                 Toast.makeText(this.context, "L\'usuari no existeix", Toast.LENGTH_SHORT).show()
             }
         }
-
 
 
         binding.txtPiniciarSessioClientOblidatContrasenya.setOnClickListener {
@@ -175,59 +121,13 @@ class PantallaIniciSessioClientAdmin : Fragment() {
         startActivity(intent)
     }
 
-    private fun showAlert(msg: String) {
+    private fun showAlert(mesage: String) {
         val builder = AlertDialog.Builder(this.context)
         builder.setTitle("ERROR")
-        builder.setMessage(msg)
+        builder.setMessage(mesage)
         builder.setPositiveButton("Acceptar", null)
         val dialog: AlertDialog = builder.create()
         dialog.show()
     }
 
-    private fun getEmail(dni: String) {
-        db.collection("users").document(dni).get().addOnSuccessListener { document ->
-            if (document != null) {
-                Email = document.get("email").toString()
-                do {
-                    println(Email)
-                } while (Email == "")
-
-                println("END DO WHILE LOOP ${Email}")
-            } else {
-                Toast.makeText(this.context, "L\'usuari no existeix", Toast.LENGTH_SHORT).show()
-                Email = "null"
-            }
-        }
-        Email
-
-    }
-
-/* private fun checkUserType(dni: String, usertype: String): Boolean{
-     var user: String
-     var bool = false
-     var check = false
-     db.collection("users").document(dni).get().addOnSuccessListener { document ->
-         user = document.get("usertype").toString()
-
-         do {
-             println("USER $user")
-         }while (user == "")
-         println("USERTYPE $user")
-
-         Handler().postDelayed(
-             {
-                 bool = user == usertype
-                 check = true
-             },
-             10000
-         )
-     }
-     /*do {
-         println("Bool WHILE $bool")
-     }while (!check)*/
-     if (check){
-         println(bool)
-         return bool
-     }
- }*/
 }
