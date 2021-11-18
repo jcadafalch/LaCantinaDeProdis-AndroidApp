8b8ff2c4dfa6a12848e8f1951fa5b2ad77aa944c
ramirez.masagu.adrian <ramirez.masagu.adrian@alumnat.copernic.cat>
2021-11-18 14:12:41 +0100

    Implementació del menu + correcció d'errors.

--- app/src/main/java/cat/copernic/prodis/lacantinadeprodis/activities/AdministradorActivity.kt
+++ app/src/main/java/cat/copernic/prodis/lacantinadeprodis/activities/AdministradorActivity.kt
@@ -1,15 +1,15 @@
 package cat.copernic.prodis.lacantinadeprodis
 
+import android.content.Intent
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.view.*
 import androidx.appcompat.app.AppCompatActivity
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
+import cat.copernic.prodis.lacantinadeprodis.activities.PantallaEdicioPerfil
 import cat.copernic.prodis.lacantinadeprodis.databinding.ActivityAdministradorBinding
 import cat.copernic.prodis.lacantinadeprodis.databinding.ActivityCaixaBinding
 
@@ -24,5 +24,32 @@ class AdministradorActivity : AppCompatActivity() {
         //Fletxa enderrere i faltaria metode onSupportNavigateUp
         //NavigationUI.setupActionBarWithNavController(this, navController)
 
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.homeBtn -> {
+                val intent = Intent(this, MainActivity::class.java).apply {
+                }
+                startActivity(intent)
+                true
+            }
+            R.id.profileBttn -> {
+                val intent = Intent(this, PantallaEdicioPerfil::class.java).apply {
+                }
+                startActivity(intent)
+                true
+            }
+            R.id.logOutBttn -> {
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
--- app/src/main/java/cat/copernic/prodis/lacantinadeprodis/activities/CambrerActivity.kt
+++ app/src/main/java/cat/copernic/prodis/lacantinadeprodis/activities/CambrerActivity.kt
@@ -1,7 +1,12 @@
 package cat.copernic.prodis.lacantinadeprodis.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import cat.copernic.prodis.lacantinadeprodis.MainActivity
 import cat.copernic.prodis.lacantinadeprodis.R
 
 class CambrerActivity : AppCompatActivity() {
@@ -17,5 +22,32 @@ class CambrerActivity : AppCompatActivity() {
 
     private fun setup (username: String){
 
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.homeBtn -> {
+                val intent = Intent(this, MainActivity::class.java).apply {
+                }
+                startActivity(intent)
+                true
+            }
+            R.id.profileBttn -> {
+                val intent = Intent(this, PantallaEdicioPerfil::class.java).apply {
+                }
+                startActivity(intent)
+                true
+            }
+            R.id.logOutBttn -> {
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
