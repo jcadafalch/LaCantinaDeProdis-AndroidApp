d5de623bce7ce6e5a539ba4f6c3d01a00bf0fb6e
Jaume Cadafalch <84527588+jcadafalch@users.noreply.github.com>
2021-11-23 12:42:00 +0100

    recuperar contrasenya + registre xml estetica

--- app/src/main/java/cat/copernic/prodis/lacantinadeprodis/principal/PantallaRecuperarContrasenya1.kt
+++ app/src/main/java/cat/copernic/prodis/lacantinadeprodis/principal/PantallaRecuperarContrasenya1.kt
@@ -1,6 +1,5 @@
 package cat.copernic.prodis.lacantinadeprodis.principal
 
-import android.app.AlertDialog
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -11,15 +10,12 @@ import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
 import cat.copernic.prodis.lacantinadeprodis.R
 import cat.copernic.prodis.lacantinadeprodis.databinding.FragmentPantallaRecuperarContrasenya1Binding
-import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 
 class PantallaRecuperarContrasenya1 : Fragment() {
 
     private val db = FirebaseFirestore.getInstance()
-    private val auth = FirebaseAuth.getInstance()
     private lateinit var usertype: String
-    private lateinit var email: String
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -31,53 +27,21 @@ class PantallaRecuperarContrasenya1 : Fragment() {
         val args = PantallaRecuperarContrasenya1Args.fromBundle(requireArguments())
         usertype = args.usertype
         binding.btnPRecuperarContrasenya1Continuar.setOnClickListener {
-            println("SET ON CLICK LISTENER")
             println(binding.dtTxtPRecuperarContrasenya1DNI.text.toString())
             if (binding.dtTxtPRecuperarContrasenya1DNI.text.toString().isNotEmpty()){
-
-                getEmail(binding.dtTxtPRecuperarContrasenya1DNI.text.toString())
+                db.collection("users").get().addOnSuccessListener {result ->
+                    for (document in result){
+                        if (document.id == binding.dtTxtPRecuperarContrasenya1DNI.text.toString()) {
+                            view?.findNavController()?.navigate(PantallaRecuperarContrasenya1Directions.actionPantallaRecuperarContrasenya1ToPantallaRecuperarContrasenya2(usertype, document.get("dni").toString()))
+                        }
+                    }
+                }
 
             }else{
                 Toast.makeText(this.context, "El camp DNI està buit", Toast.LENGTH_SHORT).show()
             }
-
         }
         return binding.root
     }
 
-    private fun getEmail(dni: String){
-        var bool = false
-        db.collection("users").get().addOnSuccessListener { result ->
-        for (document in result){
-            if (document.id == dni){
-                bool = true
-                email = document.get("email").toString()
-                auth.setLanguageCode("ca")
-                auth.sendPasswordResetEmail(email).addOnCompleteListener {
-                    if (it.isSuccessful){
-                        bool = true
-                        Toast.makeText(this.context, "S\'ha enviat un correu a: $email, per restablir la contrasenya", Toast.LENGTH_SHORT).show()
-                        view?.findNavController()?.navigate(PantallaRecuperarContrasenya1Directions.actionPantallaRecuperarContrasenya1ToPantallaIniciSessioClientAdmin(usertype))
-                    }else{
-                        println("ERROR")
-                        Toast.makeText(this.context, "No s\'ha pogut enviar un correu per restablir contrasenya", Toast.LENGTH_SHORT).show()
-                    }
-                }
-            }else{
-                println("DOCUMENT ID  = " + document.id.toString())
-            }
-        }
-        }
-        if (!bool){
-            showAlert("L\'usuari no està registrat")
-        }
-    }
-    private fun showAlert(msg: String) {
-        val builder = AlertDialog.Builder(this.context)
-        builder.setTitle("ERROR")
-        builder.setMessage(msg)
-        builder.setPositiveButton("Acceptar", null)
-        val dialog: AlertDialog = builder.create()
-        dialog.show()
-    }
 }
