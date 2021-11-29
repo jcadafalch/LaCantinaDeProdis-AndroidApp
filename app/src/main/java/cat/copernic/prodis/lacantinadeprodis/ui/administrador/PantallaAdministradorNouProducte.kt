10afa8a7324067240ad4bf67a6b1cd82e0552ae6
ramirez.masagu.adrian <ramirez.masagu.adrian@alumnat.copernic.cat>
2021-11-29 08:22:31 +0100

    Pantalla afegir producte sense pujar imatges.

--- app/src/main/java/cat/copernic/prodis/lacantinadeprodis/ui/administrador/PantallaAdministradorNouProducte.kt
+++ app/src/main/java/cat/copernic/prodis/lacantinadeprodis/ui/administrador/PantallaAdministradorNouProducte.kt
@@ -5,11 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import androidx.databinding.DataBindingUtil
 import cat.copernic.prodis.lacantinadeprodis.R
 import cat.copernic.prodis.lacantinadeprodis.databinding.FragmentPantallaAdministradorNouProducteBinding
 
-class PantallaAdministradorNouProducte : Fragment() {
+class PantallaAdministradorNouProducte : Fragment(), AdapterView.OnItemSelectedListener {
+
+    lateinit var tipusProducte: String
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -19,6 +24,42 @@ class PantallaAdministradorNouProducte : Fragment() {
             inflater, R.layout.fragment_pantalla_administrador_nou_producte, container, false
         )
 
+        val spinner: Spinner = binding.spinTipusProducte
+
+        val context = this.requireContext()
+
+        ArrayAdapter.createFromResource(
+            context,
+            R.array.tipus_producte,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            spinner.adapter = adapter
+        }
+
+        spinner.onItemSelectedListener = this
+
+        binding.imgFotoCamera.setOnClickListener(){
+
+        }
+
         return binding.root
+    }
+
+    override fun onItemSelected(parent: AdapterView<*>, view: View?, pos: Int, id: Long) {
+        tipusProducte = parent.getItemAtPosition(pos).toString()
+    }
+
+    override fun onNothingSelected(parent: AdapterView<*>) {
+        showAlert("Has de seleccionar un tipus de prodcute")
+    }
+
+    private fun showAlert(message: String) {
+        val builder = androidx.appcompat.app.AlertDialog.Builder(this.requireContext())
+        builder.setTitle("¡¡¡Error!!!")
+        builder.setMessage(message)
+        builder.setPositiveButton("Aceptar", null)
+        val dialog: androidx.appcompat.app.AlertDialog = builder.create()
+        dialog.show()
     }
 }
