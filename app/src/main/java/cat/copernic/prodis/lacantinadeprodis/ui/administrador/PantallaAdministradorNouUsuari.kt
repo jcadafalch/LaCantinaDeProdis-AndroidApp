109d5279f30ab01ff43d38c48f91432790876fd1
Jaume Cadafalch <84527588+jcadafalch@users.noreply.github.com>
2021-12-01 09:14:35 +0100

    pantalla nou usuari acabada

--- app/src/main/java/cat/copernic/prodis/lacantinadeprodis/ui/administrador/PantallaAdministradorNouUsuari.kt
+++ app/src/main/java/cat/copernic/prodis/lacantinadeprodis/ui/administrador/PantallaAdministradorNouUsuari.kt
@@ -7,6 +7,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.text.isDigitsOnly
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
 import cat.copernic.prodis.lacantinadeprodis.R
@@ -27,65 +28,103 @@ class PantallaAdministradorNouUsuari : Fragment() {
         )
 
         binding.btnPNouUsuariGurardar.setOnClickListener {
-            if (binding.dtTxtPAdministradorNouUsuariPersonName.text.isNotEmpty() &&
-                binding.dtTxtPAdministradorNouUsuariPersonSurname.text.isNotEmpty()
+            if (
+                datavalids(
+                    binding.dtTxtPAdministradorNouUsuariPersonName.text.toString(),
+                    binding.dtTxtPAdministradorNouUsuariPersonSurname.text.toString(),
+                    binding.dtTxtPAdministradorNouUsuariDni.text.toString()
+                )
             ) {
                 newUser(
+                    binding.dtTxtPAdministradorNouUsuariDni.text.toString(),
                     binding.dtTxtPAdministradorNouUsuariPersonName.text.toString(),
                     binding.dtTxtPAdministradorNouUsuariPersonSurname.text.toString()
                 )
-            } else {
-                if (binding.dtTxtPAdministradorNouUsuariPersonName.text.isEmpty() &&
-                    binding.dtTxtPAdministradorNouUsuariPersonSurname.text.isEmpty()
-                ){
-                    showAlert("ERROR", "Els camps nom d\'usuari i cognoms estan buits")
-                }
-                if (binding.dtTxtPAdministradorNouUsuariPersonName.text.isEmpty()){
-                    showAlert("ERROR", "El camp nom d\'usuari està buit")
-                }else{
-                    showAlert("ERROR", "El camp cognom d\'usuari està buit")
-                }
-
             }
         }
 
         return binding.root
     }
 
-    private fun showAlert(ttl: String, msg: String) {
+    private fun newUser(dni: String, username: String, usersurname: String) {
+        db.collection("users").document(dni).get().addOnSuccessListener { document ->
+            if (!document.exists()) {
+                db.collection("users").document(dni).set(
+                    hashMapOf("username" to username, "usersurname" to usersurname, "dni" to dni)
+                ).addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        view?.findNavController()
+                            ?.navigate(PantallaAdministradorNouUsuariDirections.actionPantallaAdministradorNouUsuariToPantallaAdministradorPrincipal())
+                        Toast.makeText(
+                            this.context,
+                            "S\'ha afegit correctament l\'usuari $username $usersurname",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    } else {
+                        showAlert("Error a l'hora de afegir el nou usuari")
+                    }
+                }
+            } else {
+                showAlert("Aquest usuari ja existeix")
+            }
+        }
+
+    }
+
+    private fun datavalids(
+        nom: String,
+        cognom: String,
+        dni: String
+    ): Boolean {
+        var errorMessage = ""
+        var bool = true
+
+        if (nom.isEmpty()) {
+            errorMessage += "Falta introduir el nom\n"
+            bool = false
+        }
+
+        if (cognom.isEmpty()) {
+            errorMessage += "Falta introduir els cognoms\n"
+            bool = false
+        }
+
+        if (dni.isEmpty()) {
+            errorMessage += "Falta introduir el dni\n"
+            bool = false
+        } else if (!checkDni(dni)) {
+            errorMessage += "Format DNI incorrecte.\n"
+            bool = false
+        }
+
+        if (errorMessage != "") {
+            showAlert(errorMessage)
+        }
+
+        return bool
+
+    }
+
+    private fun checkDni(dni: String): Boolean {
+        val dniNum = dni.substring(0, dni.length - 1)
+        if (dni.isDigitsOnly()) {
+            println("DIGIT ONLY")
+            return false
+        }
+
+        val dniLletra = dni.substring(dni.length - 1).uppercase()
+        val lletraDni = "TRWAGMYFPDXBNJZSQVHLCKE"
+
+        return dniLletra == lletraDni[dniNum.toInt() % 23].toString()
+    }
+
+    private fun showAlert(msg: String) {
         val builder = AlertDialog.Builder(this.context)
-        builder.setTitle(ttl)
+        builder.setTitle("ERROR")
         builder.setMessage(msg)
         builder.setPositiveButton("Acceptar", null)
         val dialog: AlertDialog = builder.create()
         dialog.show()
-
-    }
-
-    private fun newUser(username: String, usersurname: String) {
-        val userName = username.replace(" ", "")
-        val userSurname = usersurname.replace(" ", "")
-        val documentid = userName+userSurname
-        db.collection("users").document(documentid).get().addOnSuccessListener { document->
-            if (!document.exists()){
-                db.collection("users").document(documentid).set(
-                    hashMapOf("username" to username, "usersurname" to usersurname)
-                ).addOnCompleteListener {
-                    if (it.isSuccessful){
-                        view?.findNavController()?.navigate(PantallaAdministradorNouUsuariDirections.actionPantallaAdministradorNouUsuariToPantallaAdministradorPrincipal())
-                        Toast.makeText(this.context, "S\'ha afegit correctament l\'usuari $username $usersurname"
-                            , Toast.LENGTH_SHORT).show()
-                    }else{
-                        showAlert(
-                            "ERROR",
-                            "Error a l'hora de afegir el nou usuari"
-                        )
-                    }
-                }
-            }else{
-                showAlert("ERROR", "Aquest usuari ja existeix")
-            }
-        }
 
     }
 
