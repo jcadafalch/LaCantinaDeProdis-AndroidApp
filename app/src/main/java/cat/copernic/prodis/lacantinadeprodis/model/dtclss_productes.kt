2be04c04c9c6c5a97d84c928fa66837dc1118358
Jaume Cadafalch <84527588+jcadafalch@users.noreply.github.com>
2021-12-23 12:48:39 +0100

    solucionat

--- app/src/main/java/cat/copernic/prodis/lacantinadeprodis/model/dtclss_productes.kt
+++ app/src/main/java/cat/copernic/prodis/lacantinadeprodis/model/dtclss_productes.kt
@@ -2,7 +2,7 @@ package cat.copernic.prodis.lacantinadeprodis.model
 
 data class dtclss_productes(
     var nom: String? = null,
-    var idProducte: String? = null,
+    var idProducte: Long? = null,
     var nomid: String? = null,
     var img: String? = null,
     var preu: Int? = null,
