5573a831c24373406c5f96a4cbb35b472409e2b9
ramirez.masagu.adrian <ramirez.masagu.adrian@alumnat.copernic.cat>
2022-01-12 12:36:15 +0100

    Comentaris pantalla seleccio atributs beguda

--- app/src/main/java/cat/copernic/prodis/lacantinadeprodis/ui/comandes/PantallaSeleccioAtributsBeguda.kt
+++ app/src/main/java/cat/copernic/prodis/lacantinadeprodis/ui/comandes/PantallaSeleccioAtributsBeguda.kt
@@ -28,6 +28,7 @@ class PantallaSeleccioAtributsBeguda : Fragment() {
 
     lateinit var idProducte: String
 
+    //Iniciem l'onCreateView
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -36,74 +37,100 @@ class PantallaSeleccioAtributsBeguda : Fragment() {
             inflater, R.layout.fragment_pantalla_seleccio_atributs_beguda, container, false
         )
 
+        //Declarem i inicialitzem la variable args per agafar les dades pasades per parametres
         var args = PantallaSeleccioAtributsBegudaArgs.fromBundle(requireArguments())
+
+        //Declarem i inicialtizem una variable per fer referencia al usuari actual
         val currentUser =
             FirebaseAuth.getInstance().currentUser
 
+        //Declarem i incialitzem una variable per guardar el dni del usuari
         var dni: String
 
+        //Declarem una corrutina per guardar els atributs de la comanda
         var tasca1: Job? = setAtributs()
 
+        //Fem que la variable idProducte sigui els arguments que hem agafat abans
         idProducte = args.idProductes
 
+        //Inicialitzem la corrutina que hem creat abans
         tasca1.let {}
 
+        //Escoltem el botó de confirmar
         binding.btnConfirmar.setOnClickListener { view: View ->
+            //Cridem a la funció per setejar els atributs
             setAtributs()
 
+            //Anem a la colecció de users i agafem les dades
             db.collection("users").get().addOnSuccessListener { result ->
                 for (document in result) {
+                    //Fem que si l'email del usuari actual es igual al email de del document,
+                    //el dni será l'id del document actual que estem mirant
                     if (currentUser?.email.toString() == document.get("email").toString()) {
                         dni = document.id
+                        //Anem a la colecció comandes i agafem les dades
                         db.collection("comandes").get().addOnSuccessListener { result ->
                             for (document in result) {
+                                //Fem que si el valor "user" del document que estem mirant es igual al valor
+                                // del dni que hem agafat abans, actualitzarem les dades de comanda
+                                //amb els atributs que hem agafat amb la funció setAtributs
                                 if (dni.equals(document.get("user"))) {
                                     db.collection("comandes").document(document.id)
                                         .collection("productes").document().set(
-                                        hashMapOf(
-                                            "idProducte" to idProducte,
-                                            "emportar" to perEmportar,
-                                            "scure" to atributs
-                                        ) as Map<String, Any>
-                                    )
+                                            hashMapOf(
+                                                "idProducte" to idProducte,
+                                                "emportar" to perEmportar,
+                                                "scure" to atributs
+                                            ) as Map<String, Any>
+                                        )
                                 }
                             }
                         }
                     }
                 }
             }
+            //Anem a la pantalla de selecció tipus producte
             view.findNavController()
                 .navigate(
                     PantallaSeleccioAtributsBegudaDirections.actionPantallaSeleccioAtributsBegudaToPantallaSeleccioTipusProducte()
                 )
         }
 
+        //Escoltem al botó de tornar enrrere
         binding.btnTornaEnrerre.setOnClickListener {
+            //Fem que torni a la pantalla anterior
             findNavController().popBackStack()
         }
 
         return binding.root
     }
 
+    //Funció per setejar els atributs de la comanda
     fun setAtributs() = GlobalScope.launch(Dispatchers.Main) {
         withContext(Dispatchers.IO) {
+            //Escoltem el radio group per saber quin atribut han siguit seleccionats
             binding.rarioGroupAtributs
                 .setOnCheckedChangeListener { _, checkedId ->
                     when (checkedId) {
+                        //Si el radio sense sucre ha siguit seleccionat, el valor d'atributs será "SS"
                         R.id.radioSenseSucre -> {
                             atributs = "SS"
                         }
+                        //Si el radio de sucre blanc ha sigut seleccionat, el valor d'atributs será "SB"
                         R.id.radioSucreBlanc -> {
                             atributs = "SB"
                         }
+                        //Si el radio de sucre moré ha sigut seleccionat, el valor d'atributs será "SM"
                         R.id.radioSucreMore -> {
                             atributs = "SM"
                         }
+                        //Si el radio de sacarina ha siguit seleccionat, el valor d'atributs será "SC"
                         R.id.radioSacarina -> {
                             atributs = "SC"
                         }
                     }
                 }
+            //El valor de per emportar cambiará si el checkbox si está marcat o no
             perEmportar = binding.checkPerEmportar.isChecked
         }
     }
